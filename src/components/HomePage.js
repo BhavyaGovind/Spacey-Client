f15dd9ff2bbc8b17f67f12d844a6aef736622d18
Bhavya Govind <bhavyasanthu@Bhavyas-MacBook-Pro.local>
2021-05-24 14:17:31 +1000

    checking router

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Homepage extends Component {
+  render() {
+    return (
+      <div class="home">
+      This is spacey website
+      </div>
+    );
+  }
+}
+export default Homepage;
